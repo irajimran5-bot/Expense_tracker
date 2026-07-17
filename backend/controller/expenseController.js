6a8ec9ae2a4dcbaf1676f2316681558cf0162a6c
Iraj Imran <irajimran5@gmail.com>
2026-07-17 00:56:28 -0700

    Added update and delete functionality

--- backend/controller/expenseController.js
+++ backend/controller/expenseController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Expense from "../models/Expense.js";
 export const getExpenses=async(req,res)=>{
     try{
@@ -16,5 +17,24 @@ export const addExpense=async(req,res)=>{
     }catch(error){
         res.status(400).json({message:error.message});
         
+    }
+}
+export const deleteExpense=async(req,res)=>{
+    try{
+        const id=req.params.id;
+        const deleteexpense=await Expense.findByIdAndDelete(id);
+        res.status(200).json(deleteexpense);
+    }catch(error){
+        res.status(404).json({message:error.message});
+    }
+}
+export const updateExpense=async(req,res)=>{
+    try{
+        const id=req.params.id;
+        const{title,amount,category}=req.body;
+        const updateexpense=await Expense.findByIdAndUpdate(id,{title,amount,category},{new:true});
+        res.status(200).json(updateexpense)
+    }catch(error){
+        res.status(404).json({message:error.message});
     }
 }
