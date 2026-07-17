a87265355876bbdcb5739e16eb7d97172bd08a69
Iraj Imran <irajimran5@gmail.com>
2026-07-17 00:24:10 -0700

    Linked server.js with controllers

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,6 +1,6 @@
-const express=require("express");
+import express from "express";
 import { getExpenses,addExpense } from "../controller/expenseController.js";
-const router=express.router();
+const router=express.Router();
 router.get("/",getExpenses);
 router.post("/",addExpense);
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import expenseRoutes from "./routes/expenseRoutes.js";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
@@ -5,6 +6,7 @@ dotenv.config();
 connectDB();
 const app=express();
 app.use(express.json());
+app.use("/api/expenses",expenseRoutes);
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`Server running successfully on PORT: ${PORT}`)
