1725697f648fba1eceeeb649b71a502210259bf7
Iraj Imran <irajimran5@gmail.com>
2026-07-17 00:43:04 -0700

    Updated routes

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { getExpenses,addExpense } from "../controller/expenseController.js";
+import { getExpenses,addExpense, deleteExpense,updateExpense } from "../controller/expenseController.js";
 const router=express.Router();
 router.get("/",getExpenses);
 router.post("/",addExpense);
+router.delete(":/id",deleteExpense);
+router.put(":/id",updateExpense);
 export default router;
