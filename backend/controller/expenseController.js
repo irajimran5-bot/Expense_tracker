fcc6131ac0c5db9d5142cca2632ad4575aafe8bf
Iraj Imran <irajimran5@gmail.com>
2026-07-17 23:50:08 -0700

    Integrated controller

--- backend/controller/expenseController.js
+++ backend/controller/expenseController.js
@@ -1,40 +1,40 @@
 import mongoose from "mongoose";
 import Expense from "../models/Expense.js";
-export const getExpenses=async(req,res)=>{
+export const getExpenses=async(req,res,next)=>{
     try{
         const expenses=await Expense.find();
         res.status(200).json(expenses);
 
     }catch(error){
-        res.status(500).json({message:error.message});
+        next(error);
     }
 }
-export const addExpense=async(req,res)=>{
+export const addExpense=async(req,res,next)=>{
     try{
         const{title,amount,category}=req.body;
         const newExpense=await Expense.create({title,amount,category});
         res.status(201).json(newExpense);
     }catch(error){
-        res.status(400).json({message:error.message});
+        next(error);
         
     }
 }
-export const deleteExpense=async(req,res)=>{
+export const deleteExpense=async(req,res,next)=>{
     try{
         const id=req.params.id;
         const deleteexpense=await Expense.findByIdAndDelete(id);
         res.status(200).json(deleteexpense);
     }catch(error){
-        res.status(404).json({message:error.message});
+        next(error);
     }
 }
-export const updateExpense=async(req,res)=>{
+export const updateExpense=async(req,res,next)=>{
     try{
         const id=req.params.id;
         const{title,amount,category}=req.body;
         const updateexpense=await Expense.findByIdAndUpdate(id,{title,amount,category},{new:true});
         res.status(200).json(updateexpense)
     }catch(error){
-        res.status(404).json({message:error.message});
+        next(error);
     }
 }
