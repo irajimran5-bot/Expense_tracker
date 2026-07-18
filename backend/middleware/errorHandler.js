f635481f639ff421570cdbcc4fb83383445bb4e8
Iraj Imran <irajimran5@gmail.com>
2026-07-17 23:45:53 -0700

    Created error handlers

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,10 @@
+const errorHandler=(err,req,res,next)=>{
+    console.error("ERROR detected",err.stack||err.message);
+    const statusCode=res.statusCode==200?500:res.statusCode;
+    res.status(statusCode).json({
+        success:false,
+        message:err.message||"Something went wrong on the backend. ",
+        stack:process.env.NODE_ENV==="production"?null:err.stack,
+    });
+};
+module.exports={errorHandler};
