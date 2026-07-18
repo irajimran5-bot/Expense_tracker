58347af042fbc2609615a989c72855636378205c
Iraj Imran <irajimran5@gmail.com>
2026-07-17 23:52:40 -0700

    Completed middleware and request validations

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import expenseRoutes from "./routes/expenseRoutes.js";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 dotenv.config();
 connectDB();
 const app=express();
 app.use(express.json());
 app.use("/api/expenses",expenseRoutes);
+app.use(errorHandler);
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`Server running successfully on PORT: ${PORT}`)
