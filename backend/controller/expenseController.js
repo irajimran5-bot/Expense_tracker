6d5031082330f6d0100c7719e04b71cf81a6e6a4
Iraj Imran <irajimran5@gmail.com>
2026-07-20 12:28:40 -0700

    Minor changes

--- backend/controller/expenseController.js
+++ backend/controller/expenseController.js
@@ -25,7 +25,6 @@ export const getExpenses=async(req,res,next)=>{
         }
         const expenses=await Expense.find(queryObj);
         res.status(200).json(expenses);
-        const{page,limit,sort}=req.query;
         const page=Number(req.query.page)||1;
         const limit=Number(req.query.limit)||10;
         const skip=(page-1)*limit;
