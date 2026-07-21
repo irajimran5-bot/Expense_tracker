f34264b142a31ff922a64cfcb7f75c2c5ceff0ec
Iraj Imran <irajimran5@gmail.com>
2026-07-21 11:38:21 -0700

    Created middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+export const protect=async(req,res,next)=>{
+    try{
+        let token;
+        if(req.headers.authorization&&req.headers.authorization.startWish("Bearer")){
+            token=req.headers.authorization.split(" ")[1];
+        }
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:"No token provided. Access DENIED.",
+            });
+        }
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select("-password");
+
+        if (!req.user) {
+            return res.status(401).json({
+                success: false,
+                message: "User no longer exists.",
+            });
+        }
+        next();
+    }catch(error){
+        res.status(401).json({
+        success: false,
+        message: "Invalid or expired token.",
+        });
+    }
+}
