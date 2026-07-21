ef5cd4da5f86137461fa60e4a1ce2f04bbfe4311
Iraj Imran <irajimran5@gmail.com>
2026-07-21 11:40:20 -0700

    Connected user with expense

--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -1,5 +1,10 @@
 import mongoose, { Schema } from "mongoose";
 const expenseSchema=new mongoose.Schema({
+    user:{
+        type=mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
     title:{
         type:String,
         required:true,
