885aa1329b5c010dbde014b972aee6782687816a
Iraj Imran <irajimran5@gmail.com>
2026-07-21 11:50:42 -0700

    Minor changes

--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 const expenseSchema=new mongoose.Schema({
     user:{
-        type=mongoose.Schema.Types.ObjectId,
+        type:mongoose.Schema.Types.ObjectId,
         ref:"User",
         required:true,
     },
