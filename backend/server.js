a3d398aac9d8c589f7e7815edfcc2bae0af25cbc
Iraj Imran <irajimran5@gmail.com>
2026-07-22 12:02:04 -0700

    Installed cors package

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import expenseRoutes from "./routes/expenseRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 dotenv.config();
 connectDB();
 const app=express();
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/expenses",expenseRoutes);
 app.use("/api/auth",authRoutes);
