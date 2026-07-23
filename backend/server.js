7d212cfebf52c680f9a6bac7368882fbc0a241d8
Iraj Imran <irajimran5@gmail.com>
2026-07-23 12:12:22 -0700

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,36 @@
-import expenseRoutes from "./routes/expenseRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import serverless from "serverless-http";
+import expenseRoutes from "./routes/expenseRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import connectDB from "./config/db.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+
 dotenv.config();
+
 connectDB();
-const app=express();
-app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true
-}));
+
+const app = express();
+
+app.use(
+  cors({
+    origin: true, 
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-app.use("/api/expenses",expenseRoutes);
-app.use("/api/auth",authRoutes);
+
+app.use("/api/expenses", expenseRoutes);
+app.use("/api/auth", authRoutes);
+
 app.use(errorHandler);
-const PORT=process.env.PORT||5000;
-app.listen(PORT,()=>{
-    console.log(`Server running successfully on PORT: ${PORT}`)
-});
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running successfully on PORT: ${PORT}`);
+  });
+}
+export const handler = serverless(app);
