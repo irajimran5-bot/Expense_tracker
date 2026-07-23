23dddd42e14c513c368f779d5c39777e0aa88c37
Iraj Imran <irajimran5@gmail.com>
2026-07-23 10:40:14 -0700

    Updated theme of the website

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans:['"Plus Jakarta Sans"','sans-serif'],
+      },
+      colors:{
+        brand: {
+           50: '#f5f3ff',
+          100: '#ede9fe',
+          500: '#8b5cf6',
+          600: '#7c3aed', 
+          700: '#6d28d9',
+        },
+        cyanAccent:{
+          500:'#06b6d4',
+          600: '#0891b2',
+        }
+      }
+    },
   },
   plugins: [],
 }
