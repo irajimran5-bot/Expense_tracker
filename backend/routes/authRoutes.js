8be71a94f520665fe467e56c822747241c64c897
Iraj Imran <irajimran5@gmail.com>
2026-07-23 16:01:57 -0700

    Final changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,33 +1,9 @@
 import express from "express";
-import { registerUser, loginUser } from "../controller/authController.js";
-import { protect } from "../middleware/authMiddleware.js";
-import User from "../models/User.js";
+import { register, login } from "../controller/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.put("/update-income", protect, async (req, res) => {
-  try {
-    const { totalIncome } = req.body;
 
-    const updatedUser = await User.findByIdAndUpdate(
-      req.user.id,
-      { totalIncome },
-      { new: true }
-    ).select("-password");
-
-    if (!updatedUser) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    res.json({
-      message: "Income updated successfully",
-      user: updatedUser,
-    });
-  } catch (error) {
-    console.error("Update income error:", error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
 export default router;
