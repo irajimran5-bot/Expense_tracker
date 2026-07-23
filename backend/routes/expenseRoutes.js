ad6e4d0a610e6ad6bf511b0698cf5e6ef7648096
Iraj Imran <irajimran5@gmail.com>
2026-07-23 16:40:21 -0700

    Updated routes

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { getExpenses,addExpense, deleteExpense,updateExpense,getExpenseStats,updateIncome } from "../controller/expenseController.js";
+import { getExpenses, addExpense, deleteExpense, updateExpense, getExpenseStats, updateIncome } from "../controller/expenseController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { validateExpense } from "../middleware/validator.js";
-import User from "../models/User.js";
-const router=express.Router();
+
+const router = express.Router();
+
 router.post("/", protect, validateExpense, addExpense);
 router.get("/", protect, getExpenses);
-router.delete("/:id", protect, deleteExpense);
-router.put("/:id", protect, validateExpense, updateExpense);
 router.get("/stats", protect, getExpenseStats);
 router.put("/update-income", protect, updateIncome);
+
+router.delete("/:id", protect, deleteExpense);
+router.put("/:id", protect, validateExpense, updateExpense);
+
 export default router;
